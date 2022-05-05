628db93fac7d8dc50477cc775a45cceba362e520
TheNewKid956 <100213265+TheNewKid956@users.noreply.github.com>
2022-05-04 19:57:30 -0500

    Pause

---
+++ Secrets of the Sky/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerStats : MonoBehaviour
+{
+    public static int health = 1;
+    public static bool canMove = true;
+    public static bool canRotate = true;
+
+    void Update()
+    {
+        if (health <= 0)
+        {
+            canMove = false;
+            canRotate = false;
+            StartCoroutine(Death());
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            health--;
+        }
+    }
+
+    private IEnumerator Death()
+    {
+        yield return new WaitForSeconds(1f);
+        health = 5;
+        SceneManager.LoadScene(0);
+        canMove = true;
+        canRotate = true;
+    }
+}
